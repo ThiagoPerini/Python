924a18d624889b91d37eb89f9b4d9cef0a33419c
Thiago Perini <145079557+ThiagoPerini@users.noreply.github.com>
2023-09-18 22:28:14 -0300

    Largura, Altura e Área
    Após o usuário digitar a largura e altura o programa calculará a área, o programa se baseia em pintura de uma parede, sendo que a cada 2m é necessário 1 litro de tinta, no final da execução o programa mostrará a quantidade necessária baseado na área.

---
+++ Largura, Altura e Área.py
@@ -0,0 +1,6 @@
+largura = float(input('Digite a largura da parede: '))
+altura = float(input('Digite a altura da parede: '))
+
+area = largura * altura
+
+print(f'A area da parede com as medidas informadas foram de {area:.2f} metros quadrados e será necessário {area/2} litros de tinta')
